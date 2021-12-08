eed3e1de3d9bbd5a256fe5677183a5100d87b81c
cristophgaray <49699546+cristophgaray@users.noreply.github.com>
2021-12-08 16:30:20 -0600

    requests with wget and header and parameters

---
+++ wget_req.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget --header="Host: www.consar.gob.mx" \
+--header="Content-Length: 451" \
+--header="Cache-Control: max-age=0" \
+--header="Upgrade-Insecure-Requests: 1" \
+--header="Origin: http://www.consar.gob.mx" \
+--header="Content-Type: application/x-www-form-urlencoded" \
+--header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36" \
+--header="Accept: text/html&application/xhtml+xml&application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" \
+--header="Referer: http://www.consar.gob.mx/gobmx/aplicativo/siset/CuadroInicial.aspx?md=17" \
+--header="Accept-Encoding: gzip, deflate" \
+--header="Accept-Language: en-US,en;q=0.9" \
+--header="Cookie: ASP.NET_SessionId=a1lc4oz0mwqkzralg1g0sxfs; _ga=GA1.3.1937790533.1638665682; _gid=GA1.3.1971339304.1638665682; _gat=1" \
+--header="Connection: close" \
+--post-data='__EVENTTARGET=ctl00%24ContentPlaceHolder1%24lnkBtn_Excel&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTIyMjE1NjExOA9kFgJmD2QWAgIFD2QWAgIBD2QWAgIBD2QWBgIDDw8WAh4EVGV4dAUKQ29taXNpb25lc2RkAgUPDxYCHwAFI0NvbWlzaW9uZXMgZGUgbGFzIFNpZWZvcmVzIELDoXNpY2FzZGQCBw8PFgIfAAUeKEFsIGNpZXJyZSBkZSBvY3R1YnJlIGRlIDIwMjEpZGRkgu1vKrDmYs7dZxDCNnbyy16wCoU%3D&__VIEWSTATEGENERATOR=A7375DEB&__EVENTVALIDATION=%2FwEWBAK9%2FI7pCgKpgLXcCwLW6br9DwLW6ebvC3jJjDIa%2BTSMq4fWOEPMc%2FhCs0z2' 'http://www.consar.gob.mx/gobmx/aplicativo/siset/CuadroInicial.aspx?md=17'
